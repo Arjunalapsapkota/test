b55d1b175dd66ec4573e11dbecf8c25e89a34a01
arjunalapsapkota <arjunalapsapkota@gmail.com>
2019-01-26 14:13:20 -0800

    Test BOilerPlate

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import "./App.css";
+import Home from "./components/Home";
+import MySiftz from "./components/MySiftz";
+import Search from "./components/Search";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">Change this Everytime you run on heroku</p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            path="/mySiftz"
+            // component={this.state.isloggedin ? MySiftz : Login}
+            component={MySiftz}
+          />
+          <Route
+            path="/search"
+            // component={this.state.isloggedin ? Search : Login}
+            component={Search}
+          />
+          <Route exact path="/signup" component={SignUp} />
+
+          <Route
+            path="/login"
+            // component={this.state.isloggedin ? Login : Login}
+            component={Login}
+          />
+          <Route exact path="*" component={Home} />
+        </Switch>
+      </Router>
     );
   }
 }
